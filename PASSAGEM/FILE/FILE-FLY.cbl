8873e6fca99a2c5c7a02053d26f24b8586d9115f
João Pedro <jplopes5555@gmail.com>
2024-12-03 11:43:03 +0000

    atualização

--- PASSAGEM/FILE/FILE-FLY.cbl
+++ PASSAGEM/FILE/FILE-FLY.cbl
@@ -9,23 +9,24 @@
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
            SELECT INPUT-FILE ASSIGN TO "PASSAGEM/FILE/data.txt"
-               ORGANIZATION IS SEQUENTIAL.
+               ORGANIZATION IS LINE SEQUENTIAL.
               
            SELECT OUTPUT-FILE ASSIGN TO "PASSAGEM/FILE/output-data.txt"
-               ORGANIZATION IS SEQUENTIAL. 
+               ORGANIZATION IS LINE SEQUENTIAL. 
                
        DATA DIVISION.
        
        FILE SECTION.
        FD INPUT-FILE.
-       01 INPUT-RECORD.
-           05 INPUT-NAME           PIC X(15).
-           05 INPUT-AGE            PIC 9(2).
-           05 INPUT-NATIONALITY    PIC A(13).
-           05 INPUT-OBJDANGER      PIC A(7).
-           05 INPUT-TICKET         PIC 9(11).
-           05 INPUT-BAGS           PIC 9(11).
-           05 INPUT-SEAT           PIC X(7).
+       01 INPUT-RECORD             PIC X(67).
+           
+      *    05 INPUT-NAME           PIC X(15).
+      *    05 INPUT-AGE            PIC 9(2).
+      *    05 INPUT-NATIONALITY    PIC A(13).
+      *    05 INPUT-OBJDANGER      PIC A(7).
+      *    05 INPUT-TICKET         PIC 9(11).
+      *    05 INPUT-BAGS           PIC 9(11).
+      *    05 INPUT-SEAT           PIC X(7).
 
        FD OUTPUT-FILE.
       *01 OUTPUT-RECORD.
@@ -37,42 +38,45 @@
       *    05 OUTPUT-BAGS          PIC 9(11).
       *    05 OUTPUT-SEAT          PIC X(7).
        01 OUTPUT-TICKET-MESSAGE.
-           05 OUTPUT-MESSAGE       PIC X(135) VALUES ZEROES.
+           05 OUTPUT-MESSAGE       PIC X(135).
 
 
        WORKING-STORAGE SECTION.
-           
-       01 TICKET.
-           05 VAL-TICKET           PIC 9(5)V9(2).
-           05 FAST-TRACK.
-               10 VAL-FAST-TRACK   PIC 9(5)V9(2).
-               88 B-FAST-TRACK     VALUES "Y", "YES", "SIM", "S", 
-                   "s".
-           05 BAGS.
-               10 VAL-BAGS         PIC 9(5).
-               10 CHK-BAGS         PIC 9(1).
-               10 TOTAL-BAGS       PIC 9(5).
-           05 SEAT.
-               10 SEAT-CODE        PIC X(1).
-                   88 B-SEAT           VALUES "Y", "YES", "SIM", "S", 
-                   "s".
-               10 VAL-SEAT         PIC 9(5)V9(2).
-               10 TOTAL-SEAT       PIC 9(5).
-           05 TOTAL-TICKET         PIC 9(6).        
-       01 USER.
-           05 NAME-USER            PIC X(10).
-           05 AGE                  PIC 9(2).
-           05 NATIONALITY          PIC A(10).
-           05 OBJ-DANGER           PIC X(1).
-               88 B-OBJ-DANGEROUS  VALUES "Y", "YES", "SIM", "S", 
-                   "s".       
+       01 INPUT-NAME        PIC X(15).
+       01 INPUT-AGE         PIC 9(3).
+       01 INPUT-NATIONALITY PIC X(13).
+       01 INPUT-OBJDANGER   PIC X(7).
+       01 INPUT-TICKET      PIC 9(11).
+       01 INPUT-BAGS        PIC 9(11).
+       01 INPUT-SEAT        PIC X(7).
+      *01 TICKET.
+      *    05 VAL-TICKET           PIC 9(5)V9(2).
+      *    05 FAST-TRACK.
+      *        10 VAL-FAST-TRACK   PIC 9(5)V9(2).
+      *        88 B-FAST-TRACK     VALUES "Y", "YES", "SIM", "S", 
+      *            "s".
+      *    05 BAGS.
+      *        10 VAL-BAGS         PIC 9(5).
+      *        10 CHK-BAGS         PIC 9(1).
+      *        10 TOTAL-BAGS       PIC 9(5).
+      *    05 SEAT.
+      *        10 SEAT-CODE        PIC X(1).
+      *            88 B-SEAT           VALUES "Y", "YES", "SIM", "S", 
+      *            "s".
+      *        10 VAL-SEAT         PIC 9(5)V9(2).
+      *        10 TOTAL-SEAT       PIC 9(5).
+      *    05 TOTAL-TICKET         PIC 9(6).        
+      *01 USER.
+      *    05 NAME-USER            PIC X(10).
+      *    05 AGE                  PIC 9(2).
+      *    05 NATIONALITY          PIC A(10).
+      *    05 OBJ-DANGER           PIC X(1).
+      *        88 B-OBJ-DANGEROUS  VALUES "Y", "YES", "SIM", "S", 
+      *            "s".       
        01 END-FILE                 PIC X(1).
+       01 FIRST-CHARACTER          PIC X(1).
        PROCEDURE DIVISION.
-       INITIALIZE USER.
-       INITIALIZE TICKET.
-           MOVE 1000 TO VAL-TICKET.
-           MOVE 30 TO VAL-BAGS.
-           MOVE 20 TO VAL-SEAT.
+
            OPEN INPUT INPUT-FILE
                 OUTPUT OUTPUT-FILE.
 
@@ -81,6 +85,39 @@
                    AT END
                        MOVE "S" TO END-FILE
                    NOT AT END
+                       
+      *                MOVE INPUT-RECORD(1:15) TO INPUT-NAME       *> Nome
+      *                MOVE INPUT-RECORD(16:3) TO INPUT-AGE        *> Idade
+      *                MOVE INPUT-RECORD(19:13)TO INPUT-NATIONALITY *> Nacionalidade
+      *                MOVE INPUT-RECORD(32:7) TO INPUT-OBJDANGER  *> Objeto perigoso
+      *                MOVE INPUT-RECORD(39:11) TO INPUT-TICKET     *> Bilhete
+      *                MOVE INPUT-RECORD(50:11) TO INPUT-BAGS       *> Malas
+      *                MOVE INPUT-RECORD(61:7) TO INPUT-SEAT       *> Lugar
+      *                
+      *                DISPLAY ">" INPUT-NAME "<"
+      *                DISPLAY ">" INPUT-AGE "<"
+      *                DISPLAY ">" INPUT-NATIONALITY "<"
+      *                DISPLAY ">" INPUT-OBJDANGER "<"
+      *                DISPLAY ">" INPUT-TICKET "<"
+      *                DISPLAY ">" INPUT-BAGS "<"
+      *                DISPLAY ">" INPUT-SEAT "<"
+
+                       UNSTRING INPUT-RECORD 
+                           INTO INPUT-NAME
+                                INPUT-AGE
+                                INPUT-NATIONALITY
+                                INPUT-OBJDANGER
+                                INPUT-TICKET
+                                INPUT-BAGS
+                                INPUT-SEAT
+
+      *                DISPLAY ">" INPUT-NAME "<"
+      *                DISPLAY ">" INPUT-AGE "<"
+      *                DISPLAY ">" INPUT-NATIONALITY "<"
+      *                DISPLAY ">" INPUT-OBJDANGER "<"
+      *                DISPLAY ">" INPUT-TICKET "<"
+      *                DISPLAY ">" INPUT-BAGS "<"
+      *                DISPLAY ">" INPUT-SEAT "<"
 
       *                MOVE INPUT-NAME TO OUTPUT-NAME
       *                MOVE INPUT-AGE TO OUTPUT-AGE
@@ -91,20 +128,46 @@
       *                MOVE INPUT-SEAT TO OUTPUT-SEAT
 
       *                WRITE OUTPUT-RECORD
+      ******************************************************************    
+      *    ADICIONANDO O NOME DO PASSSAGEIRO
+      ******************************************************************
+                  STRING "O passageiro com o nome de " DELIMITED BY SIZE
+                          INPUT-NAME DELIMITED BY SIZE
+                          INTO OUTPUT-MESSAGE
 
-      *           STRING "O passageiro com o nome de " DELIMITED BY SIZE
-      *                        INPUT-NAME DELIMITED BY SIZE
+      ******************************************************************    
+      *    ADICIONANDO A IDADE DO PASSSAGEIRO
+      ******************************************************************
+                  DISPLAY OUTPUT-MESSAGE
+
+                   STRING OUTPUT-MESSAGE
+                          "< com a idade de " DELIMITED BY SIZE
+                          INPUT-AGE DELIMITED BY SPACE
+                          INTO OUTPUT-MESSAGE
+
+
+                  DISPLAY OUTPUT-MESSAGE
+
+
+      *         STRING "O passageiro com o nome de " DELIMITED BY SIZE
+      *                  INPUT-NAME DELIMITED BY SIZE
+      *                  "<com a idade de >" DELIMITED BY SIZE
+      *                  INPUT-AGE DELIMITED BY SPACE
+      *    "< é autorizado a viajar com um valor total de passagem de ",
+      *                        DELIMITED BY SIZE
+      *                        INPUT-TICKET DELIMITED BY SIZE
+      *                        "!" DELIMITED BY SIZE
       *                        INTO OUTPUT-MESSAGE
+                   STRING OUTPUT-MESSAGE
+                         " é autorizado a viajar com um valor total de", 
+                          "passagem de "
+                          DELIMITED BY SIZE
+                          INPUT-TICKET DELIMITED BY SIZE
+                          "!" DELIMITED BY SIZE
+                          INTO OUTPUT-MESSAGE
 
-                STRING " O passageiro com o nome de >" DELIMITED BY SIZE
-                               INPUT-NAME DELIMITED BY SIZE
-                               "<com a idade de >" DELIMITED BY SIZE
-                               INPUT-AGE DELIMITED BY SIZE
-           "< é autorizado a viajar com um valor total de passagem de ",
-                               DELIMITED BY SIZE
-                               INPUT-TICKET DELIMITED BY SIZE
-                               "!" DELIMITED BY SIZE
-                               INTO OUTPUT-MESSAGE
+
+
                                
                        END-STRING
       *                ADD 20 TO INPUT-TICKET
@@ -119,7 +182,6 @@
       *                END-IF
       *                ADD INPUT-AGE TO OUTPUT-MESSAGE
                        WRITE OUTPUT-TICKET-MESSAGE
-      *                
                END-READ
            END-PERFORM
             CLOSE INPUT-FILE
